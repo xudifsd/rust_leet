260b7664441435c9a060dad8024161285bcc959e
Di Xu <xudifsd@gmail.com>
2019-03-22 14:18:28 +0800

    simplified 98

--- src/validate_binary_search_tree.rs
+++ src/validate_binary_search_tree.rs
@@ -6,31 +6,29 @@ use crate::TreeNode;
 
 impl Solution {
     pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        fn helper(root: Option<Rc<RefCell<TreeNode>>>, low: i64, high: i64) -> bool {
-            match root {
-                None => true,
-                Some(r) => {
-                    let cell = r.borrow();
-                    let val = cell.val as i64;
+        fn helper(root: &TreeNode, low: i64, high: i64) -> bool {
+            let val = root.val as i64;
 
-                    if val >= high || val <= low {
-                        return false;
-                    }
-                    if let Some(l) = &cell.left {
-                        if !helper(Some(Rc::clone(l)), low, val) {
-                            return false;
-                        }
-                    }
-                    if let Some(r) = &cell.right {
-                        if !helper(Some(Rc::clone(r)), val, high) {
-                            return false;
-                        }
-                    }
-                    true
+            if val >= high || val <= low {
+                return false;
+            }
+            if let Some(l) = &root.left {
+                if !helper(&l.borrow(), low, val) {
+                    return false;
                 }
             }
+            if let Some(r) = &root.right {
+                if !helper(&r.borrow(), val, high) {
+                    return false;
+                }
+            }
+            true
         }
 
-        helper(root, std::i64::MIN, std::i64::MAX)
+        if let Some(root) = root {
+            helper(&root.borrow(), std::i64::MIN, std::i64::MAX)
+        } else {
+            true
+        }
     }
 }
